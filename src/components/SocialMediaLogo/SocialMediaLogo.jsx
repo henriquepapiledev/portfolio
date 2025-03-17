063ba975eb42c6031e931e6f1843d34b5afb8278
Henrique Papile <hpapile@gmail.com>
2025-03-17 16:06:43 -0300

    Criação do componente SocialMediaLogo

---
+++ src/components/SocialMediaLogo/SocialMediaLogo.jsx
@@ -0,0 +1,9 @@
+const SocialMediaLogo = ({ href, target, img }) => {
+  return (
+    <a className="block w-9 h-9" href={href} target={target}>
+      {img}
+    </a>
+  );
+};
+
+export default SocialMediaLogo;
