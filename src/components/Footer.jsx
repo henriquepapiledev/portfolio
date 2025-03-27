e82d43faf84f1ec900c6a2240811875eaa61a942
Henrique Papile <hpapile@gmail.com>
2025-03-27 13:13:15 -0300

    Ajustando css do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import Logo from './Logo/Logo';
 
 const Footer = () => {
   return (
-    <footer className="pt-14 pb-14 border-t-[1.5px] border-grey-bg">
+    <footer className="pt-14 pb-32 lg:pb-14 border-t-[1.5px] border-grey-bg">
       <div className="container mx-auto pl-4 pr-4">
-        <div className="flex items-center flex-col gap-5">
+        <div className="flex items-center justify-between gap-5">
           <Logo />
           <p className="font-light text-sm text-grey">
             <span className="font-bold">Henrique Papile</span> © 2025
